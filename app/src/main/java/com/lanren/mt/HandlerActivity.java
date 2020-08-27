0569871fcaa428cf756a99626c522bfbd924e27b
liuchanglinag <15563883889@163.com>
2020-08-27 12:01:07 +0800

    Handler实现倒计时功能

---
+++ app/src/main/java/com/lanren/mt/HandlerActivity.java
@@ -0,0 +1,75 @@
+package com.lanren.mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HandlerActivity extends AppCompatActivity {
+
+    private Handler mHandler;
+    private TextView mTime;
+    private Button mStart;
+    private Button mReset;
+    private int countTime = 10;
+
+    private static final int START_TIME = 1;
+    private static final int RESET_TIME = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler);
+
+        initView();
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == START_TIME){
+                    if (countTime > 0){
+                        countTime -= 1;
+                        mTime.setText(countTime + "");
+                        mHandler.sendEmptyMessageDelayed(START_TIME,1000);
+                    }else {
+                        Toast.makeText(HandlerActivity.this,"倒计时结束",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                if (msg.arg1 == RESET_TIME){
+                    mHandler.removeMessages(START_TIME);
+                    countTime = 10;
+                    mTime.setText(countTime + "");
+                }
+            }
+        };
+    }
+
+    private void initView() {
+        mTime = findViewById(R.id.time);
+        mStart = findViewById(R.id.start);
+        mReset = findViewById(R.id.reset);
+
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHandler.sendEmptyMessageDelayed(START_TIME,1000);
+            }
+        });
+
+        mReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message message = new Message();
+                message.arg1 = RESET_TIME;
+                mHandler.sendMessage(message);
+            }
+        });
+    }
+}
--- app/src/main/java/com/lanren/mt/MainActivity.java
+++ app/src/main/java/com/lanren/mt/MainActivity.java
@@ -22,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.handler).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HandlerActivity.class));
+            }
+        });
     }
 }
